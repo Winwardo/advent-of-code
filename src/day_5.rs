bfa6857ba6e52b8881777c32bd7e96b74a0346a2
Topher Winward <hi@topher.io>
2015-12-17 18:53:10 +0000

    Contains 3 vowels skeleton

--- src/day_5.rs
+++ src/day_5.rs
@@ -2,6 +2,10 @@ pub fn is_nice(input: &str) -> bool {
     return false;
 }
 
+fn contains_three_vowels(input: &str) -> bool {
+    return false;
+}
+
 #[cfg(test)]
 mod test {
     use super::*;
@@ -29,5 +33,30 @@ mod test {
     #[test]
     fn naughty_example_3() {
         assert_eq!(false, is_nice(&"dvszwmarrgswjxmb"));
+    }
+
+    #[test]
+    fn contains_three_vowels_true_1() {
+        assert_eq!(true, is_nice(&"aei"));
+    }
+
+    #[test]
+    fn contains_three_vowels_true_2() {
+        assert_eq!(true, is_nice(&"xazegov"));
+    }
+
+    #[test]
+    fn contains_three_vowels_true_3() {
+        assert_eq!(true, is_nice(&"aeiouaeiouaeiou"));
+    }
+
+    #[test]
+    fn contains_three_vowels_false_1() {
+        assert_eq!(false, is_nice(&"ae"));
+    }
+
+    #[test]
+    fn contains_three_vowels_false_2() {
+        assert_eq!(false, is_nice(&"xazegv"));
     }
 }
