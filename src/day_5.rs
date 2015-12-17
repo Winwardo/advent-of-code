49841b1282acaee05fb09e845362d3597746aab1
Topher Winward <hi@topher.io>
2015-12-17 19:48:56 +0000

    Contains 3 vowels

--- src/day_5.rs
+++ src/day_5.rs
@@ -2,8 +2,29 @@ pub fn is_nice(input: &str) -> bool {
     return false;
 }
 
-fn contains_three_vowels(input: &str) -> bool {
-    return false;
+pub fn contains_three_vowels(input: &str) -> bool {
+    return vowel_count(input) >= 3;
+}
+
+pub fn vowel_count(input: &str) -> u8 {
+	let mut count = 0;
+    for c in input.chars() {
+        if is_vowel(&c) {
+            count += 1;
+        }
+    }
+    return count;
+}
+
+pub fn is_vowel(input: &char) -> bool {
+    return match *input {
+        'a' => true,
+        'e' => true,
+        'i' => true,
+        'o' => true,
+        'u' => true,
+        _ => false,
+    };
 }
 
 #[cfg(test)]
@@ -36,27 +57,62 @@ mod test {
     }
 
     #[test]
+    fn check_is_vowel() {
+    	assert_eq!(true, is_vowel(&'a'));
+    }
+
+    #[test]
+    fn check_is_not_vowel() {
+    	assert_eq!(false, is_vowel(&'b'));
+    }
+
+    #[test]
+    fn vowel_count_none() {
+    	assert_eq!(0, vowel_count(&"qwr"));
+    }
+
+    #[test]
+    fn vowel_count_one() {
+    	assert_eq!(1, vowel_count(&"a"));
+    }
+
+    #[test]
+    fn vowel_count_two_different() {
+    	assert_eq!(2, vowel_count(&"ae"));
+    }
+
+    #[test]
+    fn vowel_count_two_same() {
+    	assert_eq!(2, vowel_count(&"aa"));
+    }
+
+    #[test]
+    fn vowel_count_all_vowels() {
+    	assert_eq!(5, vowel_count(&"aeiou"));
+    }
+
+    #[test]
     fn contains_three_vowels_true_1() {
-        assert_eq!(true, is_nice(&"aei"));
+        assert_eq!(true, contains_three_vowels(&"aei"));
     }
 
     #[test]
     fn contains_three_vowels_true_2() {
-        assert_eq!(true, is_nice(&"xazegov"));
+        assert_eq!(true, contains_three_vowels(&"xazegov"));
     }
 
     #[test]
     fn contains_three_vowels_true_3() {
-        assert_eq!(true, is_nice(&"aeiouaeiouaeiou"));
+        assert_eq!(true, contains_three_vowels(&"aeiouaeiouaeiou"));
     }
 
     #[test]
     fn contains_three_vowels_false_1() {
-        assert_eq!(false, is_nice(&"ae"));
+        assert_eq!(false, contains_three_vowels(&"ae"));
     }
 
     #[test]
     fn contains_three_vowels_false_2() {
-        assert_eq!(false, is_nice(&"xazegv"));
+        assert_eq!(false, contains_three_vowels(&"xazegv"));
     }
 }
