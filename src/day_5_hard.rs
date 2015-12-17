6a4eec0b9e41665e95eaed14eda6ccf0a1c00496
Topher Winward <hi@topher.io>
2015-12-17 23:32:04 +0000

    Non overlapping pairs, slow

--- src/day_5_hard.rs
+++ src/day_5_hard.rs
@@ -19,7 +19,29 @@ pub fn is_nice(input: &str) -> bool {
 pub fn contains_two_nonoverlapping_pairs(input: &str) -> bool {
     // It contains a pair of any two letters that appears at least twice in the string without
     // overlapping, like xyxy (xy) or aabcdefgaa (aa), but not like aaa (aa, but it overlaps).
-    return false;
+    let mut chars = input.chars();
+    let mut chars_plus_one = input.chars().skip(1);
+
+    let limit = input.len();
+    for _ in 0..(limit - 3) {
+        let l1 = chars.next();
+        let r1 = chars_plus_one.next();
+
+        // Now iterate ahead to find any matches
+        let mut chars2 = chars.clone().skip(1);
+        let mut chars2_plus_one = chars_plus_one.clone().skip(1);
+
+        for _ in 0..(limit - 2) {
+            let l2 = chars2.next();
+            let r2 = chars2_plus_one.next();
+
+            if l1 == l2 && r1 == r2 {
+                return true;
+            }
+        }
+    }
+
+    false
 }
 
 pub fn contains_one_triplet(input: &str) -> bool {
@@ -30,7 +52,7 @@ pub fn contains_one_triplet(input: &str) -> bool {
     let mut chars_compare = input.chars().skip(2);
 
     let limit = input.len() - 2;
-    for x in 0..limit {
+    for _ in 0..limit {
         let l = chars.next();
         let r = chars_compare.next();
 
@@ -38,7 +60,7 @@ pub fn contains_one_triplet(input: &str) -> bool {
             return true;
         }
     }
-    
+
     false
 }
 
@@ -84,5 +106,20 @@ mod test {
     #[test]
     fn does_not_contain_one_triplet_2() {
         assert_eq!(false, contains_one_triplet(&"abcde"));
+    }
+
+    #[test]
+    fn does_contain_two_overlapping_pairs_1() {
+        assert_eq!(true, contains_two_nonoverlapping_pairs(&"xyxy"));
+    }
+
+    #[test]
+    fn does_contain_two_overlapping_pairs_2() {
+        assert_eq!(true, contains_two_nonoverlapping_pairs(&"aabcdefgaa"));
+    }
+
+    #[test]
+    fn does_not_contain_two_overlapping_pairs_1() {
+        assert_eq!(false, contains_two_nonoverlapping_pairs(&"aaa"));
     }
 }
