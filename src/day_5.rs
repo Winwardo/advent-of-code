fa360598a2c75be00557f6858c7ad6c91fed94db
Topher Winward <hi@topher.io>
2015-12-17 20:00:50 +0000

    Does contain a double character

--- src/day_5.rs
+++ src/day_5.rs
@@ -6,8 +6,21 @@ pub fn contains_three_vowels(input: &str) -> bool {
     return vowel_count(input) >= 3;
 }
 
+pub fn contains_a_double_character(input: &str) -> bool {
+	let mut last : char = '\x00';
+    for c in input.chars(){
+    	if c == last {
+    		return true;
+    	} else {
+    		last = c;
+    	}
+    }
+
+    return false;
+}
+
 pub fn vowel_count(input: &str) -> u8 {
-	let mut count = 0;
+    let mut count = 0;
     for c in input.chars() {
         if is_vowel(&c) {
             count += 1;
@@ -58,37 +71,37 @@ mod test {
 
     #[test]
     fn check_is_vowel() {
-    	assert_eq!(true, is_vowel(&'a'));
+        assert_eq!(true, is_vowel(&'a'));
     }
 
     #[test]
     fn check_is_not_vowel() {
-    	assert_eq!(false, is_vowel(&'b'));
+        assert_eq!(false, is_vowel(&'b'));
     }
 
     #[test]
     fn vowel_count_none() {
-    	assert_eq!(0, vowel_count(&"qwr"));
+        assert_eq!(0, vowel_count(&"qwr"));
     }
 
     #[test]
     fn vowel_count_one() {
-    	assert_eq!(1, vowel_count(&"a"));
+        assert_eq!(1, vowel_count(&"a"));
     }
 
     #[test]
     fn vowel_count_two_different() {
-    	assert_eq!(2, vowel_count(&"ae"));
+        assert_eq!(2, vowel_count(&"ae"));
     }
 
     #[test]
     fn vowel_count_two_same() {
-    	assert_eq!(2, vowel_count(&"aa"));
+        assert_eq!(2, vowel_count(&"aa"));
     }
 
     #[test]
     fn vowel_count_all_vowels() {
-    	assert_eq!(5, vowel_count(&"aeiou"));
+        assert_eq!(5, vowel_count(&"aeiou"));
     }
 
     #[test]
@@ -114,5 +127,20 @@ mod test {
     #[test]
     fn contains_three_vowels_false_2() {
         assert_eq!(false, contains_three_vowels(&"xazegv"));
+    }
+
+    #[test]
+    fn does_contain_a_double_character_1() {
+        assert_eq!(true, contains_a_double_character(&"abcdde"));
+    }
+
+    #[test]
+    fn does_contain_a_double_character_2() {
+        assert_eq!(true, contains_a_double_character(&"aabbccdd"));
+    }
+
+    #[test]
+    fn does_not_contain_a_double_character_1() {
+        assert_eq!(false, contains_a_double_character(&"abcde"));
     }
 }
