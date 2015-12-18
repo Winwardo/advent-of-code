74e886fd03a083263e36c2256034c0f591e37516
Topher Winward <hi@topher.io>
2015-12-18 16:11:02 +0000

    Attempt expansion

--- src/day_7.rs
+++ src/day_7.rs
@@ -8,31 +8,37 @@ pub fn print_answer() {
     let mut circuit = Circuit::new();
 
     for line in input {
-        println!("{:?}", line);
+        //println!("{:?}", line);
         circuit.run_instruction(&line);
-        println!("{:?}", circuit.get("lx"));
+        //println!("{:?}", circuit.get("lx"));
     }
 
+    // let answer = 7;
     let answer = circuit.get("a");
     println!("{:?}", answer);
 }
 
 pub struct Circuit {
-    wires: HashMap<String, u16>,
+    wires2: HashMap<String, String>,
+    wire_vals: HashMap<String, u16>,
 }
 
 impl Circuit {
     pub fn new() -> Circuit {
-        Circuit { wires: HashMap::new() }
+        Circuit {
+            wires2: HashMap::new(),
+            wire_vals: HashMap::new(),
+        }
     }
 
-    pub fn get(&self, key: &str) -> u16 {
-        let r = self.wires.get(key);
-
-        match r {
+    pub fn get(&mut self, key: &str) -> u16 {
+        // Check memoization list
+        match self.wire_vals.get_mut(key) {
             Some(x) => *x,
-            None => 0,
-        }
+            _ => {
+                0
+            }
+        }        
     }
 
     pub fn run_instruction(&mut self, instruction: &str) {
@@ -41,28 +47,54 @@ impl Circuit {
             let lhs = cap.at(1).unwrap();
             let rhs = cap.at(2).unwrap();
 
-            self.set_val(lhs, rhs);
+            //self.set_val(lhs, rhs);
+
+
+            self.wires2.insert(rhs.to_string(), lhs.to_string());
+
+            // Try to memoize
+            match self.attempt_reduce(lhs) {
+                Some(x) => { self.wire_vals.insert(rhs.to_string(), x); },
+                _ => {}
+            };
         }
     }
 
-    fn get_actual_value(&self, given_value: &str) -> u16 {
+    fn attempt_reduce(&mut self, to_reduce: &str) -> Option<u16> {
         // Test if we're just a simple number assignment
-        match given_value.parse::<u16>() {
-            Ok(x) => return x,
+        match to_reduce.parse::<u16>() {
+            Ok(x) => { return Some(x); },
             _ => {}
         };
 
+        None
+    }
+
+    fn get_actual_value(&mut self, given_value: &str) -> u16 {
+        
+
+
+        //println!("a");
+        // let r = self.wires2.get(given_value).unwrap();
+
+        // println!("{:?} = {:?}", given_value, r);
+
+        
+
         let mut split = given_value.split(" ");
+        // println!("count {:?}", split.clone().count());
         match split.clone().count() {
             // No space (one chunk) means it's a variable assignment
             1 => self.get(given_value),
             // One space is a unary operator, and must be NOT
-            2 => !self.get_actual_value(split.skip(1).next().unwrap()),
+            2 => !self.get(split.skip(1).next().unwrap()),
             // Two spaces must be a binary operator
             3 => {
-                let left = self.get_actual_value(split.next().unwrap());
+                let left = self.get(split.next().unwrap());
                 let operator = split.next().unwrap();
-                let right = self.get_actual_value(split.next().unwrap());
+                let p = split.next();
+                let w = p.unwrap();
+                let right = self.get(w);
 
                 match operator {
                     "AND" => left & right,
@@ -75,22 +107,11 @@ impl Circuit {
             _ => 0,
         }
     }
-
-    fn set_val(&mut self, lhs: &str, wire: &str) {
-        let value = self.get_actual_value(lhs);
-        self.wires.insert(wire.to_string(), value);
-    }
 }
 
 #[cfg(test)]
 mod test {
     use super::*;
-
-    #[test]
-    fn get_nonexistent_is_zero() {
-        let circuit = Circuit::new();
-        assert_eq!(0, circuit.get("x"));
-    }
 
     #[test]
     fn set_x_is_123() {
