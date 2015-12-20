7f410d9b2f8a94d4e9bc3197b77a23a7e17faa41
Topher Winward <hi@topher.io>
2015-12-20 18:55:26 +0000

    Trivial case

--- src/day_9.rs
+++ src/day_9.rs
@@ -14,7 +14,17 @@ pub struct DistanceGraph {
 
 impl DistanceGraph {
     pub fn shortest_distance(&self) -> u32 {
-        0
+        if self.distances.len() == 0 {
+            return 0;
+        }
+
+        let mut shortest = !0; // Infinity
+        for distance in self.distances.iter() {
+            if distance.distance < shortest {
+                shortest = distance.distance;
+            }
+        }
+        shortest
     }
 }
 
@@ -23,9 +33,22 @@ mod test {
     use super::*;
 
     #[test]
-    fn trivial_case() {
+    fn empty_case() {
         let d = DistanceGraph { distances: vec![] };
 
         assert_eq!(0, d.shortest_distance());
+    }
+
+    #[test]
+    fn trivial_case() {
+        let d = DistanceGraph {
+            distances: vec![Distance {
+                                from: Location { name: "A".to_string() },
+                                to: Location { name: "B".to_string() },
+                                distance: 10,
+                            }],
+        };
+
+        assert_eq!(10, d.shortest_distance());
     }
 }
